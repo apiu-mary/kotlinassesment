5c0845731e9f57222276bc53fe506ebf6c09cd6a
apiuuagou@gmail.com <apiuagou@gmail.com>
2023-03-09 08:57:04 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,51 @@
+fun main(){
+    var result=data("Apiu","juma")
+    println(result[0])
+    var z=detail(arrayOf(10.0,20.0,30.0,40.0))
+   arrayOf("milk","bread","butter"))
+    
+
+    var calculator=Calculator()
+    calculator.add()
+    calculator.divide()
+    calculator.multipltiply()
+    calculator.subtract()
+
+
+
+
+
+}
+fun data(name1:String,name2:String):String{
+    return name1
+
+}
+fun detail(nums:Array<Double>) :Double{
+    return nums.min()
+    return nums.max()
+    return nums.average()
+
+}
+fun split(name:String){
+
+
+}
+fun array(words:Array<String>):Array<String>{
+    return words
+}
+class Calculator(){
+    fun add(){
+        println(2+4)
+    }
+    fun subtract(){
+        println(8-4)
+
+    }
+    fun  divide(){
+        println(6/3)
+    }
+    fun multipltiply(){
+        println(2*4)
+    }
+
+}
